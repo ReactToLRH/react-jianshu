de5f22b2fa4c6036cb1bd60de605ffe167519935
lrh <837233792@qq.com>
2020-02-03 22:03:57 +0800

    详情页面、路由跳转、登陆等

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import store from './store';
 import Header from './common/header';
 import Home from './pages/home';
+import Detail from './pages/detail/loadable.js';
+import Login from './pages/login';
+import Write from './pages/write';
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
         <div>
           <Header />
           <Route path='/' exact component={Home}></Route>
+          <Route path='/login' exact component={Login}></Route>
+          <Route path='/write' exact component={Write}></Route>
+          <Route path='/detail/:id' exact component={Detail}></Route>
         </div>
       </BrowserRouter>
     </Provider>
